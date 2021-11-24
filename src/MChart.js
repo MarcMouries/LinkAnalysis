228f29cc9d0b87a9a4f047ccda9f168ab3519baa
Marc Mouries <marc@mouries.net>
2021-11-23 22:52:24 -0500

    new

--- src/MChart.js
+++ src/MChart.js
@@ -1,8 +1,16 @@
+import  {MCanvas} from './MCanvas.js';
+
 export default class MChart {
+
+    objects = [];
     constructor(container) {
 
         console.log("MChart container= ");
         console.log(container);
         this.container = container;
+        mcanvas = new MCanvas(chart_container);
+        console.log("MChart objects= ");
+        console.log(objects);
+
     }
 }
