99ecde5dacc979e7e09d2f3814b8ce3f76264dfd
Marc Mouries <marc@mouries.net>
2021-11-26 09:54:50 -0500

    new

--- src/Rectangle.js
+++ src/Rectangle.js
@@ -1,14 +1,5 @@
-class Shape {
-    color = "#2793ef";
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.isSelected = false;
-    }
-    getColor() {
-        return this.color;
-    }
-}
+import Shape from '../src/Shape.js';
+
 
 export default class Rectangle extends Shape {
     constructor(x, y, width, height) {
